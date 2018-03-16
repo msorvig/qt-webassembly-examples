d6bedf927430cf4c62936a0865e5f9297afe36b4
Morten Johan Sørvig <morten.sorvig@qt.io>
2018-03-16 10:13:15 +0100

    Working pthreads example
    Use Firefox Nightly (61), remove MODULARIZE=1 which
    does not work in combination with USE_PTHREADS.

--- emscripten_pthreads/main.cpp
+++ emscripten_pthreads/main.cpp
@@ -1,22 +1,37 @@
 #include <stdio.h>
-
-#include <iostream>
-#include <string>
-#include <sstream>
-#include <algorithm>
-#include <iterator>
-#include <vector>
+#include <pthread.h>
 
 #include <emscripten.h>
 #include <emscripten/html5.h>
 
 using namespace std;
 
-int main() {
-    printf("hello, world from main()!\n");
+int value;
 
-    // fall off main: either keep running or exit with code.
-    //::emscripten_exit_with_live_runtime();
-    emscripten_force_exit(0);
+int main() {
+
+    printf("hello, world from main()!\n");
+    fprintf(stderr, "anyone?");
+    fflush(stdout); // ### stdout goes nowhere?
+
+    value = 10;
+
+    EM_ASM_({
+        console.log("Value before starting thread (expecting 10) " + $0);
+    }, value);
+
+    // start a thread which updates the value
+    pthread_t thread;
+    bool threadCreated = pthread_create(&thread, NULL, [](void *) -> void * {
+        value = 20;
+        return nullptr;
+    }, nullptr);
+
+    bool threadJoined = pthread_join(thread, NULL);
+
+    EM_ASM_({
+        console.log("Value after joining thread (expecting 20) " + $0);
+    }, value);
+
     return 0;
 }

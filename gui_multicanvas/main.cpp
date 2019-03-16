ffc93ee96554c67a027e913debcb90c3e4ad8c7a
Morten Johan Sørvig <morten.sorvig@qt.io>
2019-03-16 22:55:31 +0100

    multicanvas: support creating additional canvases
    Requires a qtloader.js with addCanvasElement/removeCanvasElement.

--- gui_multicanvas/main.cpp
+++ gui_multicanvas/main.cpp
@@ -6,8 +6,8 @@ int main(int argc, char **argv)
 {
     QGuiApplication app(argc, argv);
 
+    // Print info on initial screens
     qDebug() << "Screen count" << QGuiApplication::screens().size();
-
     int i = 0;
     for (QScreen *screen : QGuiApplication::screens()) {
         qDebug() << "Screen  " << i << screen->name();
@@ -15,11 +15,29 @@ int main(int argc, char **argv)
         ++i;
     }
 
-    for (QScreen *screen : QGuiApplication::screens()) {
+    // One window per screen
+    QHash<QScreen *, QWindow *> windows;
+    auto addScreenWindow = [&windows](QScreen *screen) {
         HelloWindow *window = new HelloWindow();
         window->setScreen(screen);
         window->showFullScreen();
-    }
+        windows.insert(screen, window);
+    };
+    auto removeScreenWindow = [&windows](QScreen *screen) {
+        delete windows.take(screen);
+    };
+
+    // Add windows for screens present at startup
+    for (QScreen *screen : QGuiApplication::screens())
+        addScreenWindow(screen);
+    
+    // Maintain windows as screens are added and removed
+    QObject::connect(&app, &QGuiApplication::screenAdded, [=](QScreen *screen){
+        addScreenWindow(screen);
+    });
+    QObject::connect(&app, &QGuiApplication::screenRemoved, [=](QScreen *screen){
+        removeScreenWindow(screen);
+    });
 
     return app.exec();
 }

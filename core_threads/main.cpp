241f8b92eca5516a9c4787749576b6ff96f8f6e7
Morten Johan Sørvig <morten.sorvig@qt.io>
2019-04-26 09:39:06 +0200

    Update core_threads example.
    Call idealThreadCount() from a worker thread as well.

--- core_threads/main.cpp
+++ core_threads/main.cpp
@@ -11,6 +11,7 @@ int main(int argc, char **argv)
 
     QThread *thread = QThread::create([] {
         qDebug() << "QThread::create thread running";
+        qDebug() << "QThread::idealThreadCount() from thread" << QThread::idealThreadCount();
     });
     thread->start();
 
@@ -21,8 +22,8 @@ int main(int argc, char **argv)
     }, nullptr);
 
     // NOTE: The follwing wait() and join() calls will block the main thread,
-    // which will (on Q_OS_WEBASSEMBLY) prevent the browser from creating the
-    // web workers for the treads, resulting in a deadlock. UNLESSS the web wokers
+    // which will (on Q_OS_WASM) prevent the browser from creating the web
+    // workers for the treads, resulting in a deadlock. UNLESSS the web wokers
     // have already been created by building with -s PTHREAD_POOL_SIZE=n.
     thread->wait();
     pthread_join(thread2, NULL);

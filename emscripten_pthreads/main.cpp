7ef33c894fae32040ed3485f0e78a59d6d13c7e3
Morten Sørvig <morten.sorvig@qt.io>
2024-04-05 16:43:22 +0200

    Test timers on thread
    It's possible to return from the thread function while
    keeping the thread alive, at least when it has a pending
    timer.
    Change-Id: Ic7768b5258c26e673a75183ee4933ef7a8a8d4cb

--- emscripten_pthreads/main.cpp
+++ emscripten_pthreads/main.cpp
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <emscripten.h>
+#include <emscripten/html5.h>
 
 using namespace std;
 
 int value = 0;
 
-int main() {
-
-    printf("hello world from main()!\n");
-
-    const int threadCount =2;
+void create_and_join()
+{
+    const int threadCount = 2;
 
     pthread_t threads[threadCount];
     for (int i = 0; i < threadCount; ++i) {
@@ -25,6 +25,39 @@ int main() {
     }
 
     printf("Value after joining thread (expecting %d) %d \n", threadCount, value);
+}
+
+
+void timer_callback(void *userData) {
+    pthread_t thread_id = pthread_self();
+    printf("Timer triggered on thread %lu!\n", (unsigned long)thread_id);
+}
+
+void create_and_timer()
+{
+    emscripten_set_timeout(timer_callback, 100, NULL);
+
+    const int threadCount = 1;
+
+    pthread_t threads[threadCount];
+    for (int i = 0; i < threadCount; ++i) {
+        pthread_create(&(threads[i]) , NULL, [](void *) -> void * {
+            printf("hello world from thread \n");
+            emscripten_set_timeout(timer_callback, 1000, NULL);
+            emscripten_set_timeout(timer_callback, 2000, NULL);
+            emscripten_set_timeout(timer_callback, 3000, NULL);
+            value += 1;
+            return nullptr;
+        }, nullptr);
+    }
+}
+
+int main() {
+
+    printf("hello world from main()!\n");
+
+    create_and_join();
+    create_and_timer();
 
     return 0;
 }
